5b38faeba3808d4e54a399d72322a0c087a1ec7e
OSLopes1 <128032769+OSLopes1@users.noreply.github.com>
2025-08-04 16:07:37 +0000

    exercise

--- src/Ch04/04_01b/CodeDemo.cpp
+++ src/Ch04/04_01b/CodeDemo.cpp
@@ -6,6 +6,13 @@
 
 int main(){
     
+    int enemies[4];
+
+    enemies[0] = 10;
+    enemies[1] = 15;
+    enemies[2] = 20;
+    enemies[3] = 25;
+
     std::cout << std::endl << std::endl;
     return 0;
 }
